16ce17dd9b7db6ec75a62e77f1138ee80647ec64
kristian307 <kristian.rhoads@yandex.ru>
2019-04-27 12:43:30 +0300

    C:/Program Files/Git/

--- src/com/mailsender/Main.java
+++ src/com/mailsender/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         MailLogic mailLogic = new MailLogic();
 
-        MailUser user = new MailUser("sdikansky96@gmail.com", "super12307");
+        MailUser user = new MailUser("", "");
 
         mailLogic.setProperties("smtp.gmail.com", 587,true,true);
         mailLogic.setUser(user);
